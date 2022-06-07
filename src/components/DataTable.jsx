06ca7dca3ce3bb137267e167e89c84d33542bd17
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-06-07 18:37:45 -0400

    Includes search by name filter :heavy_check_mark:

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -2,9 +2,15 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 function DataTable() {
-  const { data } = useContext(PlanetsContext);
+  const { data, nameFilterData, setNameFilterData } = useContext(PlanetsContext);
   // console.log(data);
-  // console.log(results);
+  function handleFilterChange({ target: { value } }) {
+    // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+    const nameFilter = data.filter((planet) => (planet.name.toLowerCase()
+    // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+      .includes(value.toLowerCase())));
+    setNameFilterData(nameFilter);
+  }
   // Eu tentei usar o slice e o splice mas ficou um código grande
   const title = [
     'name',
@@ -22,33 +28,40 @@ function DataTable() {
     'url',
   ];
   return (
-    // <h1>Hello Universe!</h1>
-    <table>
-      <tbody>
-        <tr>
-          {title.map((planet, index) => (
-            <th key={ index }>{ planet }</th>
-          ))}
-        </tr>
-        {data.map((planet, index) => (
-          <tr key={ index }>
-            <td>{ planet.name }</td>
-            <td>{ planet.rotation_period }</td>
-            <td>{ planet.orbital_period }</td>
-            <td>{ planet.diameter }</td>
-            <td>{ planet.climate }</td>
-            <td>{ planet.gravity }</td>
-            <td>{ planet.terrain }</td>
-            <td>{ planet.surface_water }</td>
-            <td>{ planet.population }</td>
-            <td>{ planet.films }</td>
-            <td>{ planet.created }</td>
-            <td>{ planet.edited }</td>
-            <td>{ planet.url }</td>
+    <div>
+      <input
+        type="text"
+        data-testid="name-filter"
+        placeholder="Search"
+        onChange={ handleFilterChange }
+      />
+      <table>
+        <tbody>
+          <tr>
+            {title.map((planet, index) => (
+              <th key={ index }>{ planet }</th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+          {nameFilterData.map((planet, index) => (
+            <tr key={ index }>
+              <td>{ planet.name }</td>
+              <td>{ planet.rotation_period }</td>
+              <td>{ planet.orbital_period }</td>
+              <td>{ planet.diameter }</td>
+              <td>{ planet.climate }</td>
+              <td>{ planet.gravity }</td>
+              <td>{ planet.terrain }</td>
+              <td>{ planet.surface_water }</td>
+              <td>{ planet.population }</td>
+              <td>{ planet.films }</td>
+              <td>{ planet.created }</td>
+              <td>{ planet.edited }</td>
+              <td>{ planet.url }</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- src/context/PlanetsProvider.jsx
+++ src/context/PlanetsProvider.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import PlanetsContext from './PlanetsContext';
 import PlanetsFetchAPI from './PlanetsFetchAPI';
 
 function PlanetsProvider({ children }) {
   const [data] = PlanetsFetchAPI();
-  const contextValue = { data };
+  const [nameFilterData, setNameFilterData] = useState([]);
+  useEffect(() => { setNameFilterData(data); }, [data]);
+  const contextValue = { data, nameFilterData, setNameFilterData };
   return (
     <PlanetsContext.Provider value={ contextValue }>
       { children }
